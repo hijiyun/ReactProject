f9b61ebd2cb184185d7e73bb2b6c29faf56b6531
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-02-19 00:19:46 +0900

    list 추가 및 css 추가 커밋

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -1,17 +1,20 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  let posts = '강남 고기 맛집'
-  
+  let [글제목, 글제목변경] = useState('여자 봄 옷 추천');
+  let posts = "강남 고기 맛집 리스트";
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={ {color: 'blue', fontSize:'30px'} }>개발 Blog</div>
+        <div style={{color:'pink'}}>개발 Blog</div>
       </div>
-      <img src={ logo }></img>
-      <h4>{ posts }</h4>
+      <div className="list">
+        <h3>{ 글제목 }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
