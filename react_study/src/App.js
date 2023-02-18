2b67b6563f33a1c8f4fedbb254f8b372152a7b79
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-02-18 23:01:47 +0900

    app.js 커밋

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = '강남 고기 맛집'
+  function 함수() {
+    return 100
+  }
+  
   return (
     <div className="App">
       <div className="black-nav">
-        <div>개발 Blog</div>
+        <div style={ {color: 'blue'} }>개발 Blog</div>
       </div>
+      <img src={ logo }></img>
+      <h4>{ posts }</h4>
     </div>
   );
 }
