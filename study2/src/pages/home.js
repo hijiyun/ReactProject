3b8861d2827889e8ada134bf1b8e76037dc0fe41
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-04 15:59:49 +0900

    메뉴만들기

---
+++ study2/src/pages/home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return <h2>home화면 입니다.</h2>
+};
+
+export default Home;
+
+// import React, { Component } from "react";
+
+// class Home extends Component{
+//     render() {
+//         return <h1>Home화면 입니다.</h1>
+//     }
+// }
+
+// export default Home;
