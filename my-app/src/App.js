2681bbebad71458163f9249b8fb5958aa7b8c27c
jiyunsisgod <114905530+hijiyun@users.noreply.github.com>
2023-03-06 14:42:37 +0900

    useEffect 활용

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,14 +5,19 @@ function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event); 
+  const onChange = (event) => setKeyword(event.target.value); 
   console.log("I run all the time");
   useEffect(() => {
     console.log("Call the API...")
   }, []);
+  useEffect(()=> {
+    if (keyword !== "" && keyword.length > 5){
+      console.log("Search For.. ", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <input value={setKeyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me!</button>
     </div>
