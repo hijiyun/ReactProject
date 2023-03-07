9b8be600f3f7ad6da1255b7def488abb8adc8905
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-07 23:26:35 +0900

    input이랑 버튼 생성

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,9 +1,24 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">
-    <h1>안녕</h1>
-  </div>;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) =>
+    event.preventDefault();
+  console.log(toDo);
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Writer your to do .."
+        /><hr/>
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
