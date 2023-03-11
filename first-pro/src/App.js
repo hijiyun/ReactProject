af12d8a3451cbec4970443b2d54918a512ffe494
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-11 15:11:24 +0900

    usestate 공부...

--- first-pro/src/App.js
+++ first-pro/src/App.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import './App.css';
 import Box from "./component/Box"
 
 function App() {
   let counter = 0
+  const [counter2, setCounter2] = useState(0);
   const increase = () => {
-    counter = counter + 1
-    console.log("counter는:",counter )
+    counter = counter + 1;
+    setCounter2(counter2 + 1);
+    console.log("counter는: ", counter, "counter2는:" , counter2);
   }
   return (
     <div>
@@ -14,6 +17,7 @@ function App() {
       <Box name="로제" num={3} />
       <Box name="제니" num={4} /> */}
       <div>{counter}</div>
+      <div>state:{ counter2 }</div>
       <button onClick={increase}>클릭 !</button>
     </div>
   );
