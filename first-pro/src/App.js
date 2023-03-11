3fc61a59f30e8c65078034544da4bd28057341a4
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-11 13:57:35 +0900

    props 배우기

--- first-pro/src/App.js
+++ first-pro/src/App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
+import Box from "./component/Box"
 
 function App() {
+  let counter = 0
+  const increase = () => {
+    counter = counter + 1
+    console.log("counter는:",counter )
+  }
   return (
-    <div className="box">
-      BOX1
-      <p>제니</p>
+    <div>
+      {/* <Box name="리사" num={1} />
+      <Box name="지수" num={2} />
+      <Box name="로제" num={3} />
+      <Box name="제니" num={4} /> */}
+      <div>{counter}</div>
+      <button onClick={increase}>클릭 !</button>
     </div>
   );
 }
