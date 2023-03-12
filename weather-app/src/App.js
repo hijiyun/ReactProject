8009ce575b9cb4ea125d8359e5384c892b873170
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-12 23:55:30 +0900

    현재위치 가져옴

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+  const getCrrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => { 
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+      console.log("현재위치: ",lat,lon)
+    });
+  }
+  useEffect(() => {
+    getCrrentLocation()
+  },[])
   return (
     <div>
       <h1>Hi</h1>
