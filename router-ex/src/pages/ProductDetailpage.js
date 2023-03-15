fd8f85f5581b8618516691596afa93d45868073a
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-15 22:41:01 +0900

    id값 불러오기 url

--- router-ex/src/pages/ProductDetailpage.js
+++ router-ex/src/pages/ProductDetailpage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetailpage = () => {
+  const { id } = useParams();
+  console.log("ppp", id);
   return (
     <div>
-      <h1>Show all product detail</h1>
+      <h1>Show all product detail { id }</h1>
     </div>
   )
 }
