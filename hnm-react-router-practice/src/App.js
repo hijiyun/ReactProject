def2cf185929802bf0fa5429778fe9c9b7864add
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-16 00:10:41 +0900

    Navbar 만들기

--- hnm-react-router-practice/src/App.js
+++ hnm-react-router-practice/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import ProductAllPage from './pages/ProductAllPage';
-import Loginpage from './pages/Loginpage';
 import ProductsDeailPage from './pages/ProductsDeailPage';
+import Login from './pages/Login';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path='/' element={<ProductAllPage />}></Route>
-        <Route path='/login' element={<Loginpage />}></Route>
+        <Route path='/login' element={<Login />}></Route>
         <Route path='/products/:id' element={<ProductsDeailPage />}></Route>
       </Routes>
     </div>
---
+++ hnm-react-router-practice/src/component/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { fauser } from "@fortawesome/fa-solid fa-user-plus";
+
+const Navbar = () => {
+    return (
+      <div>
+        <div>
+                <FontAwesomeIcon icon={ fauser} />
+        </div>
+
+        <div></div>
+
+        <div></div>
+      </div>
+    );
+}
+
+export default Navbar
