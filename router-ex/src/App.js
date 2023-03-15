202cd9c6c113ab92dd823853d990ff5891c688c1
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-15 23:20:27 +0900

    useState를 사용

--- router-ex/src/App.js
+++ router-ex/src/App.js
@@ -3,11 +3,19 @@ import "./pages/Homepage"
 import "./pages/Aboutpage"
 import Homepage from './pages/Homepage';
 import Aboutpage from './pages/Aboutpage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Productpage from './pages/Productpage';
 import ProductDetailpage from './pages/ProductDetailpage';
+import Loginpage from './pages/Loginpage';
+import { useState } from 'react';
+import Userpage from './pages/Userpage';
 
 function App() {
+  const [authenticate, setauthenticate] = useState(false)
+  const PrivateRoute = () => {
+    return authenticate == true?<Userpage />:<Navigate to="/login" />
+  }
+
   return (
     <div>
       <Routes>
@@ -15,6 +23,8 @@ function App() {
         <Route path="/about" element={<Aboutpage />}></Route>
         <Route path="/products" element={<Productpage />}></Route>
         <Route path="/products/:id" element={<ProductDetailpage />}></Route>
+        <Route path="/login" element={<Loginpage />}></Route>
+        <Route path="/user" element={<PrivateRoute />}></Route>
       </Routes>
     </div>
   );
