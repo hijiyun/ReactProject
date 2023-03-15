0ad5ac31709bcbf2e5274872d9f9dc5a96e75aa7
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-15 23:36:31 +0900

    라우터 준비 끝

---
+++ hnm-react-router-practice/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='' element={}></Route>
+        <Route path='' element={}></Route>
+        <Route path='' element={}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
