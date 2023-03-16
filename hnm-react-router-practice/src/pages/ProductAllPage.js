bf9b4713eac271c017db6a8560ec0002bc5cf474
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-17 00:26:22 +0900

    프로덕트 페이지 db.json api 가져옴 및 프로덕트 카드 페이지 생성

--- hnm-react-router-practice/src/pages/ProductAllPage.js
+++ hnm-react-router-practice/src/pages/ProductAllPage.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProductAllPage = () => {
+  const [productList, setproductList] = useState([]); 
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setproductList(data);
+  }
+  useEffect(() => {
+    getProducts()
+  },[])
   return (
       <div>
           
