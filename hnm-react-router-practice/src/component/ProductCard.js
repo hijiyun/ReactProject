b5199cd42d264607e3183af15e9d00a9b4934afc
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-17 01:05:49 +0900

    map() 사용했는데 api가 안 불러와짐

--- hnm-react-router-practice/src/component/ProductCard.js
+++ hnm-react-router-practice/src/component/ProductCard.js
@@ -1,14 +1,14 @@
     import React from 'react'
 
-    const ProductCard = () => {
+    const ProductCard = (items) => {
     return (
-    <div>
-        <img src="" alt="card" />
+      <div>
+        <img src="{items?.img}" alt="card" />
         <div>Conscious choice</div>
-        <div>벨티드 트윌 코드</div>
-        <div>₩99900</div>
+        <div>{items?.title}</div>
+        <div>₩{items?.price}</div>
         <div>신제품</div>
-    </div>
+      </div>
     );
     }
 
--- hnm-react-router-practice/src/pages/ProductAllPage.js
+++ hnm-react-router-practice/src/pages/ProductAllPage.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
+import { Container, Row, Col } from "react-bootstrap";
 
 const ProductAllPage = () => {
   const [productList, setproductList] = useState([]); 
@@ -12,10 +14,19 @@ const ProductAllPage = () => {
     getProducts()
   },[])
   return (
-      <div>
-          
+    <div>
+      <Container>
+        <Row>
+          {productList.map((items) => (
+            <Col md={3} sm={12} key={items.id}>
+              <ProductCard item={ items} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <ProductCard />
     </div>
-  )
+  );
 }
 
 export default ProductAllPage
