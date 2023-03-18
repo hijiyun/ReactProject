999af66b8d9384fd354044a925a09d0734c51f29
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-18 23:55:29 +0900

    app.js 수정, app.css 수정

--- hnm-react-router-practice/src/App.js
+++ hnm-react-router-practice/src/App.js
@@ -1,18 +1,39 @@
-import './App.css';
-import { Route, Routes } from "react-router-dom";
-import ProductAllPage from './pages/ProductAllPage';
-import ProductsDeailPage from './pages/ProductsDeailPage';
-import Login from './pages/Login';
-import Navbar from './component/Navbar';
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import PrivateRoute from "./Route/PrivateRoute";
+import { useDispatch, useSelector } from "react-redux";
+import AdminProduct from "./page/AdminProduct";
+import { useLocation } from "react-router-dom";
 
+import { userActions } from "./action/userAction";
+import ProductDetail from "./page/ProductDetail";
 function App() {
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
+  useEffect(() => {
+    dispatch(userActions.loginWithToken());
+  }, []);
   return (
     <div>
-      <Navbar />
+      {!location.pathname.includes("admin") && <Navbar user={user} />}
       <Routes>
-        <Route path='/' element={<ProductAllPage />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/products/:id' element={<ProductsDeailPage />}></Route>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route
+          path="/admin/product"
+          element={
+            <PrivateRoute>
+              <AdminProduct />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
