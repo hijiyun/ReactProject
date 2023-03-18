517e54caea6ed05f7df9f2d673aea614bf8171f5
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-19 00:22:41 +0900

    Reducer  생성 및 추가 및 보수작업 !

---
+++ hnm-react-router-practice/src/reducer/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userReducer";
+import productReducer from "./productReducer";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    product: productReducer,
+  },
+});
+export default store;
