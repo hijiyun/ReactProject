f30623b586097c12aa67d317b3fd0221a72125b4
jiyunsisgod <114905530+hijiyun@users.noreply.github.com>
2023-03-20 19:38:25 +0900

    class component

--- react_study/src/App2.js
+++ react_study/src/App2.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import ReactPropTypes from 'prop-types'
 
 
-const App2 = () => {
-  return (
+class App2 extends React.Component{
+  render(){
+  const state ={
+      counter: 0
+    };
+    return 
     <div>
-      dkssyd
+        <h1>The number is:  {this.state.counter}</h1>
     </div>
-  )
+  }
 }
+    
 
 export default App2
