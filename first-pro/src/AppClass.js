a080228725461cdd21ad8b4ab885c85b29626c41
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-26 22:43:04 +0900

    클래스 컴포넌트 공부

--- first-pro/src/AppClass.js
+++ first-pro/src/AppClass.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react'
+import BoxClass from './component/BoxClass';
 
 export default class AppClass extends Component {
-    constructor(props) {
-        super(props);
-        this.setstate = {
-            counter2: 0,
-            num: 1,
-            value: 0
-        };
-    }
-    increase = () => {
-        this.setState({ counter2: this.state.counter2 + 1, value: this.state.value + 1, });
-    };
+  constructor(props) {
+    super(props) // 여기까지가 class component에서 쓰이는 state 기본세팅
+    this.state = {
+      counter2: 0,
+      num: 1,
+      value:3
+    }; // 이 컴포넌트 안에는 state가 3개다. (오브젝트 안에다가)
+  }
 
+  increase = () => {
+    this.setState({counter2:this.state.counter2 + 1, value:this.state.value +3})
+  };
 
   render() {
     return (
       <div>
         <div>state:{this.state.counter2}</div>
         <button onClick={this.increase}>클릭 !</button>
+        <BoxClass num={ this.state.value} />
       </div>
     );
   }
