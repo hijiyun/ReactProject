47914eee0194f34eeef1caa976820bdc1cbd0ab8
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-03-28 22:44:34 +0900

    css 하기

--- hnm-router/src/component/Navbar.js
+++ hnm-router/src/component/Navbar.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faBagShopping } from "@  /free-solid-svg-icons";
-import { faBell } from "@  /free-solid-svg-icons";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => {
   return (
     <div>
-      <div className="header">
+      <div className='nav'>
         <div header-left>
           고객서비스 &nbsp;&nbsp; 뉴스레터 &nbsp;&nbsp; 매장찾기 &nbsp;&nbsp;
           <FontAwesomeIcon icon={faBell} />
         </div>
-        <div>로고</div>
+
+        <div className='nav-section'>
+          <img 
+            width={100}
+            src="https://blog.kakaocdn.net/dn/Yt80C/btqDeJAYUBo/JQbTuukRladq2AUOeqgiEK/img.jpg" alt="hnm로고" />
+        </div>
+
         <div header-right>
           <div>
             <FontAwesomeIcon icon={faUser} /> &nbsp; 로그인 &nbsp;&nbsp;&nbsp;
@@ -24,6 +30,7 @@ const Navbar = () => {
             <FontAwesomeIcon icon={faBagShopping} /> &nbsp; 쇼핑백
           </div>
         </div>
+
       </div>
 
       <div></div>
