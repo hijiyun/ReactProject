e71e0ac67e748b0f719ce78e11584211a050791a
PgmJun <tkrhkfld123@naver.com>
2023-03-29 12:03:54 +0900

    db.json 성공 ~

--- hnm-router/src/page/ProductAll.js
+++ hnm-router/src/page/ProductAll.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const ProductAll = () => {
+  const getProducts = async()=>{
+    let url = `http://localhost:5000/products`
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log(data);
+  }
+  useEffect(()=>{
+    getProducts()
+  },[])
   return (
     <div>전체 상품 페이지</div>
   )
