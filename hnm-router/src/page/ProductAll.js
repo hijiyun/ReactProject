00110a3e095be3f89800ec768f7bc888caa0016c
PgmJun <tkrhkfld123@naver.com>
2023-03-29 12:47:57 +0900

    사진 api불러오기

--- hnm-router/src/page/ProductAll.js
+++ hnm-router/src/page/ProductAll.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
+import {Col, Container, Row} from "react-bootstrap";
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState([]);
   const getProducts = async()=>{
     let url = `http://localhost:5000/products`
     let response = await fetch(url)
     let data = await response.json()
-    console.log(data);
+    setProductList(data);
   }
   useEffect(()=>{
     getProducts()
   },[])
   return (
-    <div>전체 상품 페이지</div>
+    <div>
+      <Container>
+        {productList.map((menu)=>(
+          <Row>
+            <Col lg={3}> <ProductCard item={menu}/> </Col>
+          </Row>
+        ))}
+      </Container>
+      <ProductCard />
+    </div>
   )
 }
 
