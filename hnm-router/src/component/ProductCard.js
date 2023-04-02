0571844031cdd326a8b9cece231b36121c381686
PgmJun <tkrhkfld123@naver.com>
2023-04-02 20:41:40 +0900

    useNavigate() 함수 써서 페이지 이동

--- hnm-router/src/component/ProductCard.js
+++ hnm-router/src/component/ProductCard.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ProductCard = ({item}) => {
+  const navigate = useNavigate();
+  const showDetail=()=>{
+    navigate(`/Product/${item.id}`);
+  }
   return (
-    <div className='item-card'>
-        <img className='item-img' src={item?.img} />
-        <div>Conscious choice</div>
+    <div className='item-card' onClick={showDetail}>
+        <img className='item-img' src={item?.img} alt="item.img"/>
+        <div>{item?.choice}</div>
         <div>{item?.title}</div>
         <div>{item?.price}</div>
         <div>신제품</div>
