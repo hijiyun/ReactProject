216a0095010734dd1f1aba8bae0a8100c55ee604
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-04-08 22:43:57 +0900

    ContactForm commit

--- phonenum-book/src/component/ContactForm.js
+++ phonenum-book/src/component/ContactForm.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
 const ContactForm = () => {
   const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState(0);
-  const {contact} = useSelector(state => state)
+  const [phoneNumber, setPhoneNumber] = useState(null);
+  const { contact } = useSelector(state => state);
   const dispatch = useDispatch();
 
   const addContact = (event) => {
@@ -14,30 +14,32 @@ const ContactForm = () => {
     if (contact.find((item) => item.name === name || item.phoneNumber === phoneNumber)) {
       alert('이미 추가된 연락처입니다.');
     } else {
-      dispatch({ type: "ADD_CONTACT", payload: { name:name, phoneNumber:phoneNumber } });
-      console.log(name,phoneNumber)
-      setName("");
-      setPhoneNumber("");
+      dispatch({ type: "ADD_CONTACT", payload: { name, phoneNumber } });
+      console.log(name, phoneNumber)
+      setName(prevName => {
+        if (prevName === name) return prevName;
+        return '';
+      });
+      setPhoneNumber('');
     }
   }
-    
 
   return (
     <div>
       <Form onSubmit={addContact}>
-      <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Label>이름</Form.Label>
-        <Form.Control type="text" placeholder="이름을 입력해 주세요" onChange={(event)=>setName(event.target.value)} />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>이름</Form.Label>
+          <Form.Control type="text" placeholder="이름을 입력해 주세요" onChange={(event) => setName(event.target.value)} />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formContact">
-        <Form.Label>전화번호</Form.Label>
-        <Form.Control type="number" placeholder="전화번호를 입력해주세요" onChange={(event)=>setPhoneNumber(event.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        추가
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formContact">
+          <Form.Label>전화번호</Form.Label>
+          <Form.Control type="number" placeholder="전화번호를 입력해주세요" onChange={(event) => setPhoneNumber(event.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          추가
+        </Button>
+      </Form>
     </div>
   )
 }
