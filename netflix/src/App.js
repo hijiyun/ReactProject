f503d1477358729bca52cd3718689d9026087ab7
jiyun <114905530+hijiyun@users.noreply.github.com>
2023-05-01 23:08:27 +0900

    Reboot App.js

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -4,14 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="titlte">
-      <h1>안녕하세요</h1>
-      <h1>저는</h1>
-      <h1>박지윤 입니다</h1>
-      <h1>낼부터 다시 시작 !!</h1>
-      <h1>개인 사정으로 인해 ..</h1>
-      <h1>진짜 시작 ..</h1>
-      <h1>어랏..</h1>
-      <h1>안뇽..</h1>
+
     </div>
   );
 }
