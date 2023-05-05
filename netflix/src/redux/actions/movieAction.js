1ebc7eaaa8b90a3a4f7ba561a8ecbab314e75bb1
PgmJun <tkrhkfld123@naver.com>
2023-05-06 00:22:47 +0900

    update await Promise.all
    Promise.all 쓰는 방법 알기

--- netflix/src/redux/actions/movieAction.js
+++ netflix/src/redux/actions/movieAction.js
@@ -1,8 +1,15 @@
 import api from "../api"
 
+const API_KEY =process.env.REACT_APP_API_KEY
 function getMovies(){
     return async (dispatch)=>{
-        const popularMovieApi = await api.get(`/movie/popular?api_key=<<api_key>>&language=en-US&page=1`)
+        const popularMovieApi = api.get(`/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+        const topRatedMovieApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
+        const upComingMovieApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
+
+       let data = await Promise.all([popularMovieApi, topRatedMovieApi, upComingMovieApi]);
+       console.log("data:", data)
+
         // let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
         // let response = await fetch(url)
         // let data = await response.json()
