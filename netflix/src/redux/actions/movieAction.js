ea8e845cf9fd3a3b1774b572c4710cd8564386d0
PgmJun <tkrhkfld123@naver.com>
2023-05-06 00:28:07 +0900

    data 따로 저장

--- netflix/src/redux/actions/movieAction.js
+++ netflix/src/redux/actions/movieAction.js
@@ -7,15 +7,10 @@ function getMovies(){
         const topRatedMovieApi = api.get(`/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`);
         const upComingMovieApi = api.get(`/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`);
 
-       let data = await Promise.all([popularMovieApi, topRatedMovieApi, upComingMovieApi]);
-       console.log("data:", data)
-
-        // let url = `https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1`
-        // let response = await fetch(url)
-        // let data = await response.json()
-
-        // let url2 = `https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1`
-        // let response2 = await fetch(url2)
+       let [popularMovies,topRatedMovies,upComingMovies] = await Promise.all([popularMovieApi, topRatedMovieApi, upComingMovieApi]);
+       console.log("popularMovies:",popularMovies)
+       console.log("topRatedMovies:",topRatedMovies)
+       console.log("upComingMovies:",upComingMovies)
     }
 }
 
